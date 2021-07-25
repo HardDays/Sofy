e6724e5944344b111095937bca69de91f5b4f045
nightwelf <nightwelf@gmail.com>
2021-07-25 19:46:14 +0500

    2.0.0+10

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -31,10 +31,46 @@ class Fonts {
 const kUrlPattern = r"(https?|http)://([-A-Z0-9.]+)(/[-A-Z0-9+&@#/%=~_|!:,.;]*)?(\?[A-Z0-9+&@#/%=~_|!:‌​,.;]*)?";
 const List<double> kInitSpeedValues = [
   4,
-  3.9, 3.8, 3.7, 3.6, 3.5, 3.4, 3.3, 3.2, 3.1, 3,
-  2.9, 2.8, 2.7, 2.6, 2.5, 2.4, 2.3, 2.2, 2.1, 2,
-  1.9, 1.8, 1.7, 1.6, 1.5, 1.4, 1.3, 1.2, 1.1, 1,
-  0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1];
+  3.9,
+  3.8,
+  3.7,
+  3.6,
+  3.5,
+  3.4,
+  3.3,
+  3.2,
+  3.1,
+  3,
+  2.9,
+  2.8,
+  2.7,
+  2.6,
+  2.5,
+  2.4,
+  2.3,
+  2.2,
+  2.1,
+  2,
+  1.9,
+  1.8,
+  1.7,
+  1.6,
+  1.5,
+  1.4,
+  1.3,
+  1.2,
+  1.1,
+  1,
+  0.9,
+  0.8,
+  0.7,
+  0.6,
+  0.5,
+  0.4,
+  0.3,
+  0.2,
+  0.1
+];
 const kSliderSpeedValue = 62;
 const kSpeedValuesPosition = 30;
 const kIsLoop = false;
@@ -207,6 +243,4 @@ class Layout {
 const String monthly_purchase_key = "Monthly access";
 const String annual_purchase_key = "Annual access";
 
-const List<int> equalizerValues = [
-  59, 108, 80, 44, 35, 28, 17, 17, 35, 28, 17, 17, 24, 48, 35, 24, 20, 17, 17, 10
-];
+const List<int> equalizerValues = [59, 108, 80, 44, 35, 28, 17, 17, 35, 28, 17, 17, 24, 48, 35, 24, 20, 17, 17, 10];
